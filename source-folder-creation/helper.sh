5b7ed068075724af162ad85a3cf47f98de8c5b37
skyzhou0 <hao.s.zhou@outlook.com>
2021-08-23 01:30:11 +0100

    adding dummy files to keep folders in place

---
+++ source-folder-creation/helper.sh
@@ -0,0 +1,6 @@
+# To Preserver the empty folder structure in Github, we can creat . files.
+
+touch src/main/java/.gitkeep
+touch src/main/resources/.gitkeep
+touch src/test/java/.gitkeep
+touch src/test/resources/.gitkeep
