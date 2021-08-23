6a276ec206d5aada8fb717c7366f655b97f660e8
skyzhou0 <hao.s.zhou@outlook.com>
2021-08-23 11:46:24 +0100

    initial commit to github

---
+++ java-application-01/helper.sh
@@ -0,0 +1,11 @@
+# 1. create source code.
+cd src/main/java
+
+mkdir -p cambridge/programming/project/
+
+# then add an "Application.java". note that we need to make sure 
+package cambridge.programming.project;
+# matches the folder structure.
+
+# 2. Maven packaging.
+mvn package
---
+++ java-application-01/src/main/java/cambridge/programming/project/Application.java
@@ -0,0 +1,16 @@
+package cambridge.programming.project;
+
+public class Application {
+    
+    public Application() {
+        System.out.println ("Inside Application");
+    }
+
+    // method main(): ALWAYS the APPLICATION entry point
+    public static void main (String[] args) {
+    	System.out.println ("Starting Application");
+	Application app = new Application();
+    }
+}
+
+// java-application-01/src/main/java/cambridge/programming/Application.java
