334b73fa2bf03f3f6e69170f86d9d0bc57289f56
skyzhou0 <hao.s.zhou@outlook.com>
2021-08-23 12:48:21 +0100

    using maven plugin.

--- java-application-01/helper.sh
+++ java-application-01/helper.sh
@@ -8,4 +8,18 @@ package cambridge.programming.project;
 # matches the folder structure.
 
 # 2. Maven packaging.
-mvn package
+mvn clean package
+
+# 3. Run the application.
+cd target 
+java -cp maven-project-package-1.0.jar cambridge.programming.project.Application
+
+
+# 4. Explore Maven Repository - "install" copy jar file and install to local maven repository use it as a cache.
+mvn clean install
+
+ls ~/.m2/repository/
+# you will find the package repository, the jar, and metadata.
+
+# 5 Commit and update
+git commit -am "using maven plugin."
--- java-application-01/src/main/java/cambridge/programming/project/Application.java
+++ java-application-01/src/main/java/cambridge/programming/project/Application.java
@@ -1,15 +1,29 @@
 package cambridge.programming.project;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     
     public Application() {
         System.out.println ("Inside Application");
     }
 
+    public void greet() {
+        List<String> greetings = new ArrayList<>();
+        greetings.add("Hello");
+
+        for (String greeting: greetings) {
+            System.out.println("Greetings: " + greetings);
+        }
+
+    }
+
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
-	Application app = new Application();
+	    Application app = new Application();
+        app.greet();
     }
 }
 
